2afa713a7d8519c88ebf422e744aa009f1c3de2d
Gilles Figuière <me@gillesfiguiere.com>
2024-11-14 11:30:43 +0100

    Augmentation de la couverture de test de UpdateQuality

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -6,12 +6,22 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
-    [Fact]
-    public void foo()
+    [Theory]
+    [InlineData("+5 Dexterity Vest", 5, 3, 4, 2 )]
+    [InlineData("Aged Brie", 5, 3, 4, 4 )]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 3, 4, 6 )]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 8, 3, 7, 5 )]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 13, 3, 12, 4 )]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", -1, 3, -2, 0 )]
+    [InlineData("+5 Dexterity Vest", -1, 3, -2, 1 )]
+    [InlineData("Aged Brie", -1, 3, -2, 5 )]
+    public void ShouldUpdateQualityForDexterityVest(string name, int sellInBefore, int qualityBefore, int sellInExpected, int qualityExpected)
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
+        IList<Item> items = new List<Item> { new Item { Name = name, SellIn = sellInBefore, Quality =qualityBefore} };
+        GildedRose app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+        Assert.Equal(name, items[0].Name);
+        Assert.Equal(sellInExpected, items[0].SellIn);
+        Assert.Equal(qualityExpected, items[0].Quality);
     }
 }
