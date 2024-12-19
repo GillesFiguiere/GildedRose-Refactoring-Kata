0a36217c62268fddc9b015481dc2c6c2f172255d
Gilles Figuière <me@gillesfiguiere.com>
2024-12-19 10:21:56 +0100

    lister les codes smell

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -13,12 +13,17 @@ public class GildedRose
 
     public void UpdateQuality()
     {
+        // TODO On ne voit pas les règles métiers
+        // exemple si c'es agedbrie je fais ça, si c'est Sulfuras, etc.
         foreach (var item in Items)
         {
+            // TODO strings en dur répétées => attention les TYPOS
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
+                // Duplication quality > 0
                 if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                 {
+                    // Duplication décrémentation
                     item.Quality = item.Quality - 1;
                 }
             }
@@ -26,10 +31,12 @@ public class GildedRose
             {
                 if (item.Quality < 50)
                 {
+                    // Duplication incrémentation
                     item.Quality = item.Quality + 1;
 
                     if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
+                        // Duplication quality < 50
                         if (item.SellIn < 11 && item.Quality < 50)
                         {
                             item.Quality = item.Quality + 1;
