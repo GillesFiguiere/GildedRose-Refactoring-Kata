d071154c4db11ea0483b1ed23c20e61cc3bb475f
Gilles Figuière <me@gillesfiguiere.com>
2024-12-19 11:33:11 +0100

    WIP remonte les condition après sellin --

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -17,46 +17,63 @@ public class GildedRose
         // exemple si c'es agedbrie je fais ça, si c'est Sulfuras, etc.
         foreach (var item in Items)
         {
+            
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn = item.SellIn - 1;
+            }
+            
             // TODO strings en dur répétées => attention les TYPOS
-            if (item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name == "Aged Brie")
             {
                 if (item.Quality < 50)
                 {
                     // Duplication incrémentation
                     item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        // Duplication quality < 50
-                        if (item.SellIn < 11 && item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-
-                        if (item.SellIn < 6 && item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
                 }
             }
-            else
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.SellIn < 0)
+                {
+                    item.Quality = 0;
+                }
+                
+                else if (item.Quality < 50)
+                {
+                    // Duplication incrémentation
+                    item.Quality = item.Quality + 1;
+
+                  
+                        // Duplication quality < 50
+                        if (item.SellIn < 10 && item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+
+                        if (item.SellIn < 5 && item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+
+                       
+
+                }
+            }
+            else if (item.Name != "Sulfuras, Hand of Ragnaros")
             {
                 // Duplication quality > 0
-                if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    // Duplication décrémentation
-                    item.Quality = item.Quality - 1;
-                }
+                
+                
+                    if (item.Quality > 0)
+                    {
+                        // Duplication décrémentation
+                        item.Quality = item.Quality - 1;
+                    }
+                
             }
 
-            if (item.Name == "Sulfuras, Hand of Ragnaros")
-            {
-            }
-            else
-            {
-                item.SellIn = item.SellIn - 1;
-            }
+           
 
             if (item.SellIn < 0)
             {
@@ -69,18 +86,11 @@ public class GildedRose
                 }
                 else
                 {
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        item.Quality = 0;
-                    }
-                    else
-                    {
                         if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                         {
                             item.Quality = item.Quality - 1;
                         }
                     }
-                }
             }
         }
     }
